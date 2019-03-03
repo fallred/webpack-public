0f18e36f1c487a01ea09bb2c1242e69b30f2cc7f
fallred <fallred@sina.com>
2019-03-03 14:06:44 +0800

    多入口

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
 // 基于node的 遵循commonjs规范的
 module.exports = {
-    entry:'./src/index.js',// 入口
+    entry: ['./src/index.js', './src/a.js'],// 入口
     output: {
-        filename: 'build.js',
+        filename: 'build.[hash].js',
         // 这个路径必须是绝对路径
         path:path.resolve('./build')
     },// 出口
@@ -15,9 +17,22 @@ module.exports = {
         hot: true
     },// 开发服务器
     module: {},//模块配置
-    plugins: [],// 插件的配置
+    plugins: [
+        new CleanWebpackPlugin(['./build']),
+        // 打包html插件
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: '珠峰架构',
+            hash: true
+            // minify: {
+            //     removeAttributeQuotes: true,
+            //     collapseWhitespace: true
+            // }
+        }),
+    ],// 插件的配置
     mode: 'development',//可以更改模式
-    resolve: {},// 配置解析
+    resolve: {}// 配置解析
 };
 // 1.在webpack中如何配置开发服务器 webpack-dev-server
+// 2. webpack插件 1将html打包到build下可以自动引入生产的js
 
